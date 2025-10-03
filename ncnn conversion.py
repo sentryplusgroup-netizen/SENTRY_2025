a688920abe362eb9a976eb9b567ea30bbd35e827
Sentry <sentryplusgroup@gmail.com>
2025-10-03 13:50:03 -0700

    ncn conversion for custom model

--- ncnn conversion.py
+++ ncnn conversion.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model 
+model = YOLO("deer_detect.pt")  # load a pretrained model 
 
 # Export the model to NCNN format
 model.export(format="ncnn", imgsz=320)  #creates yolov8n_ncnn_model  
